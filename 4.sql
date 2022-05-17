59e3b797dfff512241e138432f0785f53e8ccf21
Kushal Basak <105678287+2001-kushalbasak@users.noreply.github.com>
2022-05-17 19:24:29 +0530

    Add files via upload

---
+++ 4.sql
@@ -0,0 +1,11 @@
+/*Show everything from table*/
+SELECT * FROM `demo_table`
+
+
+/*show selected columns only show all the values from column id,name,roll,class and sec*/
+
+SELECT id,name,roll,class,sec FROM `demo_table`
+
+/*show id column as student_id*/
+
+SELECT id AS student_id FROM `demo_table` 
