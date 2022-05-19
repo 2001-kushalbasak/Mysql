a9b1029b97c9542954ad753022669a1a0388741b
Kushal Basak <105678287+2001-kushalbasak@users.noreply.github.com>
2022-05-19 23:23:33 +0530

    Add files via upload

---
+++ 5.sql
@@ -0,0 +1,19 @@
+/*select using where clause*/
+
+Select * from demo_table WHERE age = 20;
+
+Select * from demo_table WHERE age <= 20;
+
+Select * from demo_table WHERE age >= 20;
+
+Select * from demo_table WHERE age = 20;
+
+Select * from demo_table WHERE age <> 20; /* <> means not equal to*/
+
+Select * from demo_table WHERE age != 20;
+
+Select * from demo_table WHERE gender = 'male';
+
+Select * from demo_table WHERE gender = 'F';
+
+Select * from demo_table WHERE city = 'kolkata';
