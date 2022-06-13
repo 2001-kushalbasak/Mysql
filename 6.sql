84ac01ed67bf40a91b26aaa02ad2f51630a86d2d
Kushal Basak <105678287+2001-kushalbasak@users.noreply.github.com>
2022-06-14 01:41:02 +0530

    Add files via upload

---
+++ 6.sql
@@ -0,0 +1,10 @@
+CREATE TABLE s2(
+    id INT AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    name VARCHAR(255) NOT NULL,
+    adhar_card VARCHAR(255) UNIQUE,
+    city VARCHAR(255) DEFAULT 'Kolkata',
+    age INT NOT NULL CHECK(age>=18)
+    
+    );
+    
